bc04e86f4056b514f22404d13660498a820816d7
Prateek Sharma <prateekps0123@gmail.com>
2025-09-07 18:06:28 +0530

    Add GEMINI API KEY

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDpAAVkdfjDL2sevimHQEeqRtonQUSQtYo";
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
